df48a842bd5aa40eecc019e7c376bce9b7e77bcf
용제 우 <utr.pbc@gmail.com>
2022-03-17 02:15:54 +0900

    의도 실패 1차 키가 눌리는대로 씬의 변화 및 종료가 되지 않음

--- winAPI_IGame/CTitle.cpp
+++ winAPI_IGame/CTitle.cpp
@@ -2,13 +2,19 @@
 #include "CTitle.h"
 #include "CBackGround.h"
 
-
 void CTitle::Enter()
 {
 	CBackGround* m_pTitleImage = new CBackGround;
 	m_pTitleImage->Load(L"Title", L"texture\\background\\Title.png");
 	m_pTitleImage->SetPos(fPoint(0, 0));
+	m_pTitleImage->SetScale(fPoint(WINSIZEX, WINSIZEY));
 	AddObject(m_pTitleImage, GROUP_GAMEOBJ::BACKGROUND);
+
+	if (Key(VK_SPACE))
+		ChangeScn(GROUP_SCENE::STAGE_01);
+
+	else if (Key(VK_ESCAPE))
+		PostQuitMessage(0);
 }
 
 void CTitle::Exit()
